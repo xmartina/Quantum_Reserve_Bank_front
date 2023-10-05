0188f4c072a4a9d4edc4ce08d9804122c26932df
unknown <martinsprince126@gmail.com>
2023-10-04 20:11:54 -0400

    init files

--- config.php
+++ config.php
@@ -1,1 +1,12 @@
 <?php
+$siteMail = "contact@quantumreserve.online";
+const webUrl = "https://quantumreserve.online";
+const phone = "+2340000000";
+const siteName = "Quantum Reserve Bank";
+const homeUrl = "/";
+const aboutUrl = webUrl ."/about.php";
+const termsUrl = webUrl ."/terms.php";
+const faqUrl = webUrl ."/faq.php";
+const contactUrl = webUrl ."/contact.php";
+const loginUrl = "https://dashboard.quantumreserve.online/login";
+const signupUrl = "https://dashboard.quantumreserve.online/signup";
